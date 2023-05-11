80d57270d58fb1c57a02f05b0be191ea27eba672
Rajesh Sharma <dude0@dude0.local>
2023-05-11 10:34:01 +0545

    Added validator builder and update product container size

--- src/server/validator.js
+++ src/server/validator.js
@@ -4,7 +4,7 @@ export const stringValidate = (key)  => check(key, 'Should be a non empty string
 
 export const uuidValidate = (key) => check(key, 'Should be a valid uuid').trim().matches(/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/).escape();
 
-export const numberValidate = (key) => check(key, "Should be a valid number").isNumeric().notEmpty();
+export const numberValidate = (key) => check(key, "Should be a valid number").trim().notEmpty().isNumeric();
 
 export const objectValidate = (key) => check(key, "Should be a vaild object").isObject().notEmpty();
 
