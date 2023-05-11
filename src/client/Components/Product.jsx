615d3dee807e16f9fc1b6e9f2443148b879a78ef
Rajesh Sharma <dude0@dude0.local>
2023-05-11 22:33:36 +0545

    adding sliding work

--- src/client/Components/Product.jsx
+++ src/client/Components/Product.jsx
@@ -15,7 +15,7 @@ export const Product = ({ name, images = [], id, price, brand, title}) => {
     const [currentTranslate, setCurrentTranslate] = useState(0);
     const [prevTranslate, setPrevTranslate] = useState(0);
 
-    const animationRef = useRef(null);
+    const sliderRef = useRef(null);
 
     const changeImage = (index, next) => {
         if(next) {
@@ -30,15 +30,18 @@ export const Product = ({ name, images = [], id, price, brand, title}) => {
         return event.type.includes('mouse') ?  event.pageX : event.touches[0].clientX;
     }
 
-    const setSliderPosition = (e, currentTranslate) => {
-        e.target.style.transform = `translateX(${currentTranslate}px)`;
+    const setSliderPosition = (currentTranslate) => {
+        sliderRef.current.style.transform = `translateX(${currentTranslate}px)`;
     }
 
-    const setPositionByIndex = useCallback((e) => {
-        const crrT = imgIndex * -e.target.clientWidth;
-         setSliderPosition(e, crrT)
-         setPrevTranslate(currentTranslate);
-    }, [currentTranslate, imgIndex])
+
+    const  setPositionByIndex = useCallback((imgIndex, e) => {
+        const currentTranslate = imgIndex * - e.target.clientWidth;
+        setPrevTranslate(currentTranslate);
+        setSliderPosition(currentTranslate)
+      }, [currentTranslate])
+      
+
 
     const stopDefaultDrag = useCallback((e) => {
             e.preventDefault();
@@ -48,7 +51,6 @@ export const Product = ({ name, images = [], id, price, brand, title}) => {
     const touchStart = useCallback((i, e) => {
         console.log("start")
         setIsDragging(true);
-        setImgIndex(i);
         setStartPos(getPositionX(e))
     }, [])
 
@@ -56,46 +58,42 @@ export const Product = ({ name, images = [], id, price, brand, title}) => {
     const touchEnd = useCallback((e) => {
         console.log("end")
         setIsDragging(false)
-        const movedBy = currentTranslate - prevTranslate;
-        if(movedBy < -50 && imgIndex < images.length - 1) {
-            setImgIndex(prev => {
-                return  prev + 1
-                }
-              )
-        }
+        const movedBy = currentTranslate - prevTranslate
 
-        if(movedBy > 50 && imgIndex > 0) {
-            setImgIndex(prev => {
-               return  prev - 1
-             })
-        }
+        setImgIndex(prev => {
+            
+            if (movedBy < -100 && imgIndex < images.length - 1) {
+               prev= prev + 1;
+            }
+    
+            if (movedBy > 100 && imgIndex > 0) { 
+                prev = prev -1;
+            }
+
+            setPositionByIndex(prev, e);
+
+            return prev;
+        });
+
+        
 
     }, [currentTranslate, imgIndex, images, prevTranslate])
 
     const touchMove = useCallback((e)=> {
         if(isDragging) {
-           const currentPosition = getPositionX(e);
-           const crrT  = prevTranslate + currentPosition - startPos
-
-
-           const isLeft = crrT  < 0; 
-           console.log("images rect",e.target.getBoundingClientRect());
-           console.log("prev", prevTranslate, "curr", currentPosition, "start", startPos, "appar", crrT)
-           if(images.length === 1) { return }
-           if(isLeft && imgIndex === images.length - 1 || !isLeft && imgIndex === 0 ) {
-              
-           } else {
-            setSliderPosition(e, crrT);
-            setCurrentTranslate(crrT); 
-           }
+            const currentPosition = getPositionX(e)
+            const currentTranslate = prevTranslate + currentPosition - startPos;
+            console.log("Moving", currentTranslate, "prev", prevTranslate, "curr", currentPosition, "start", startPos);
+            setCurrentTranslate(currentTranslate);
+            setSliderPosition(currentTranslate);
         }
     }, [isDragging,  currentTranslate, imgIndex,  images , prevTranslate, startPos])
 
-    const touchEndThrottle = _.throttle(touchEnd, 500);
+    const touchEndThrottle = _.throttle(touchEnd, 0);
 
     return <div className="product-container">
             <div className="product">
-                <div className="images">
+                <div className="images" ref={sliderRef}>
                     {images.map((src, i) => <img 
                             onDragStart={stopDefaultDrag}
                             onTouchStart={(e) => touchStart(i, e)}
